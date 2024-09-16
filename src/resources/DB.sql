6b50421c6067951e1a1d255fcd4f2d0bf7ca4df7
Yasith Ishara Perera <yasithishara@gmail.com>
2024-09-16 15:00:28 +0530

    added all tables

--- src/resources/DB.sql
+++ src/resources/DB.sql
@@ -103,3 +103,115 @@ CREATE TABLE offer
     discount    DECIMAL(9, 2),
     is_active   BOOLEAN
 );
+
+CREATE TABLE customer_offer
+(
+    customer_id VARCHAR(20) NOT NULL REFERENCES customer (id),
+    offer_id    VARCHAR(20) NOT NULL REFERENCES offer (id),
+    CONSTRAINT pk_co PRIMARY KEY (customer_id, offer_id)
+);
+
+CREATE TABLE material
+(
+    id        VARCHAR(20) PRIMARY KEY,
+    name      VARCHAR(100) NOT NULL,
+    is_active BOOLEAN
+);
+
+CREATE TABLE stock
+(
+    material_id VARCHAR(20) PRIMARY KEY,
+    qty         DECIMAL(9, 2) NOT NULL,
+    unit        VARCHAR(10)   NOT NULL,
+    username    VARCHAR(100) REFERENCES "user" (username),
+    CONSTRAINT fk_mat FOREIGN KEY (material_id) REFERENCES material (id)
+);
+
+CREATE TABLE delivery
+(
+    order_id       VARCHAR(20) PRIMARY KEY REFERENCES customer_order (id),
+    name           VARCHAR(200) NOT NULL,
+    phone          VARCHAR(20)  NOT NULL,
+    address        VARCHAR(500) NOT NULL,
+    payment_option VARCHAR(50)  NOT NULL,
+    is_active      BOOLEAN
+);
+
+CREATE TABLE dine_in
+(
+    order_id  VARCHAR(20) PRIMARY KEY REFERENCES customer_order (id),
+    table_no  INT NOT NULL,
+    is_active BOOLEAN
+);
+
+CREATE TABLE take_away
+(
+    order_id  VARCHAR(20) PRIMARY KEY REFERENCES customer_order (id),
+    name      VARCHAR(200) NOT NULL,
+    phone     VARCHAR(20)  NOT NULL,
+    is_active BOOLEAN
+);
+
+CREATE TABLE menu_item
+(
+    id              VARCHAR(20) PRIMARY KEY,
+    name            VARCHAR(100)  NOT NULL,
+    food_type       VARCHAR(50)   NOT NULL,
+    sub_category_id VARCHAR(20) REFERENCES item_sub_category (sub_category_id),
+    portion_id      VARCHAR(20) REFERENCES portion_size (portion_id),
+    price           DECIMAL(9, 2) NOT NULL,
+    is_active       BOOLEAN
+);
+
+CREATE TABLE customer_order_detail
+(
+    item_id   VARCHAR(20) REFERENCES menu_item (id),
+    order_id  VARCHAR(20) REFERENCES customer_order (id),
+    qty       INT           NOT NULL,
+    price     DECIMAL(9, 2) NOT NULL,
+    is_active BOOLEAN,
+    CONSTRAINT pk_cod PRIMARY KEY (item_id, order_id)
+);
+
+CREATE TABLE item_material
+(
+    item_id     VARCHAR(20) REFERENCES menu_item (id),
+    material_id VARCHAR(20) REFERENCES material (id),
+    qty         DECIMAL(9, 2),
+    CONSTRAINT pk_imat PRIMARY KEY (item_id, material_id)
+);
+
+CREATE TABLE supplier_order
+(
+    id          VARCHAR(20) PRIMARY KEY,
+    supplier_id VARCHAR(20) REFERENCES supplier (id),
+    username    VARCHAR(100) REFERENCES "user" (username),
+    date        DATE NOT NULL,
+    is_active   BOOLEAN
+);
+
+CREATE TABLE supplier_order_detail
+(
+    order_id    VARCHAR(20) REFERENCES supplier_order (id),
+    material_id VARCHAR(20) REFERENCES material (id),
+    unit_price  DECIMAL(9, 2) NOT NULL,
+    qty         DECIMAL(9, 2) NOT NULL,
+    CONSTRAINT pk_sod PRIMARY KEY (order_id, material_id)
+);
+
+CREATE TABLE grn
+(
+    id                VARCHAR(20) PRIMARY KEY,
+    supplier_order_id VARCHAR(20) REFERENCES supplier_order (id),
+    date              DATE,
+    is_active         BOOLEAN
+);
+
+CREATE TABLE grn_detail
+(
+    grn_id      VARCHAR(20) REFERENCES grn (id),
+    material_id VARCHAR(20) REFERENCES material (id),
+    unit_price  DECIMAL(9, 2) NOT NULL,
+    qty         DECIMAL(9, 2) NOT NULL,
+    CONSTRAINT pk_grnd PRIMARY KEY (grn_id, material_id)
+);
